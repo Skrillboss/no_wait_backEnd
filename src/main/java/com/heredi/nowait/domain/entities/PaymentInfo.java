2d91830edd2350dd06a6c0b2734ca7acc8161955
Luis <heredialuis20@gmail.com>
2024-09-04 16:20:27 +0200

    Implementation: business and paymentInfo into user model

---
+++ src/main/java/com/heredi/nowait/domain/entities/PaymentInfo.java
@@ -0,0 +1,85 @@
+package com.heredi.nowait.domain.entities;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "paymentInfo")
+public class PaymentInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cardNumber;
+
+    @Column(nullable = false)
+    private String cardHolderName;
+
+    @Column(nullable = false)
+    private LocalDate expiryDate;
+
+    @Column(nullable = false)
+    private String cardType;
+
+    @Column(nullable = false)
+    private String cvv;
+
+    public PaymentInfo(String cardNumber, String cardHolderName, LocalDate expiryDate, String cardType, String cvv) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.expiryDate = expiryDate;
+        this.cardType = cardType;
+        this.cvv = cvv;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+}
