1c4cd1e10c8589d3871ba64ba69e1de7a2edb247
luis <heredialuis20@gmail.com>
2024-10-16 15:16:10 +0200

    Deleted: info into itemResponseDTO

--- src/main/java/com/heredi/nowait/application/item/dto/out/ItemResponseDTO.java
+++ src/main/java/com/heredi/nowait/application/item/dto/out/ItemResponseDTO.java
@@ -1,6 +1,5 @@
 package com.heredi.nowait.application.item.dto.out;
 
-import com.heredi.nowait.application.shift.dto.in.ShiftRequestDTO;
 import com.heredi.nowait.application.shift.dto.out.ShiftResponseDTO;
 import lombok.Data;
 
@@ -12,14 +11,9 @@ public class ItemResponseDTO {
     private String id;
     private String name;
     private String description;
-    private int numberPeopleWaiting;
-    private int peoplePerShift;
-    private int numberShiftsWaiting;
     private Double rating;
     private String mainImagePath;
     private String secondaryImagePath;
-    private int currentWaitingDuration; // En minutos
-    private int durationPerShifts; // En minutos
     private String status; // Representa el ItemStatus como String
     private List<ShiftResponseDTO> shifts;
 }
--- src/main/java/com/heredi/nowait/domain/item/model/Item.java
+++ src/main/java/com/heredi/nowait/domain/item/model/Item.java
@@ -1,13 +1,9 @@
 package com.heredi.nowait.domain.item.model;
 
 import com.heredi.nowait.domain.queue.model.Queue;
-import com.heredi.nowait.domain.shift.model.Shift;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.Duration;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
